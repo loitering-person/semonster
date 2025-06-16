d682d5be208bafcef35aa9fba9092459beb2e6bb
loitering-person <vannesstjie@gmail.com>
2025-06-16 11:05:37 +0900

    Revert "playerCommit"
    This reverts commit c6b8aec0a2fcea9fe664be2f7831fc0e8cb23dc6.

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
-    Player player = new Player();
-    player.drawMonsters();
-    player.showDeck();
-    //Monster monster = new Monster();
-    //System.out.println(monster.toString());
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
   }
 }
