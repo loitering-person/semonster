ed992a8617be552dd4216029671d5385f847ccbf
loitering-person <vannesstjie@gmail.com>
2025-06-16 10:25:24 +0900

    test

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMonster!");
+    Monster monster = new Monster();
   }
 }
